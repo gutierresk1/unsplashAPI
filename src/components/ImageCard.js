93500980edf1a1bc0575ab1e5984c6eed73daf4c
gutierresk1 <43971263+gutierresk1@users.noreply.github.com>
2021-01-21 14:19:47 -0500

    fine tuned a couple of things. Search should be a bit cleaner

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,22 +1,33 @@
 import React from 'react'
-
+//for each time we call upon the imagelist component, we will populate it with this component.
+//we use a class here instead of a function.
 class ImageCard extends React.Component{
-    //This component shows a div with an image insode of it
+    //This component shows a div with an image inside of it
     constructor(props) {
         super(props);
-
+        //override the prop of React.component.
+        this.state = {spans: 0}
         //instance of the variable this.imageRef
         this.imageRef = React.createRef();
     }
 
     componentDidMount() {
-        console.log(this.imageRef.current);
+        this.imageRef.current.addEventListener('load', this.setSpans);
         console.log(this.imageRef.current.clientHeight);
     }
+
+    setSpans =()=>{
+        const height = this.imageRef.current.clientHeight;
+
+        const spans = Math.ceil(height / 10); 
+
+        this.setState({ spans });
+    };
+
     render(){
         const { description, urls } = this.props.image;
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 {/*  */}
                 <img ref={this.imageRef} alt={description} src={urls.regular} />
             </div>
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -2,11 +2,14 @@ import './ImageList.css';
  import React from 'react'
  import ImageCard from './ImageCard';
  
+ //we use a function in this component, instead of a class. The prop in this case 
+ //will be images inside of the props of ImageList in App.js
  const ImageList = (props) =>{
      const images = props.images.map((image) => {
         return <ImageCard key={image.id} image={image} />
+        //This ImageCard has to props: the key and the image itself.
      });
-     
+
      return <div className="image-list">{images}</div>
  };
 
